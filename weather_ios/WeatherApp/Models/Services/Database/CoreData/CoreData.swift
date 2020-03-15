1337dee2b130acc043b24695c877205a53d47bf4
pavel <pavel.antonov1980@icloud.com>
2020-03-15 11:26:32 +0100

    project structure update

---
+++ weather_ios/WeatherApp/Models/Services/Database/CoreData/CoreData.swift
@@ -0,0 +1,13 @@
+//
+//  CoreData.swift
+//  WeatherApp
+//
+//  Created by paul on 15/03/2020.
+//  Copyright © 2020 pavel. All rights reserved.
+//
+
+import CoreData
+
+struct CoreData: StorageService {
+    
+}
